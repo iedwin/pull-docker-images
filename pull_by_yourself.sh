1510c78b71b0de95eb9e350eb68c264f22b245ef
Your Name <you@example.com>
2020-11-04 17:24:53 +0800

    clean

--- pull_by_yourself.sh
+++
@@ -1,7 +0,0 @@
-base_repo=registry.cn-shenzhen.aliyuncs.com/images_mirror
-
-docker pull gcr.io/tekton-releases/github.com/tektoncd/dashboard/cmd/dashboard@sha256:e72593477e8cb09b718b9b0b38731de5b4cb7ef0c04712f7b97f51be5c5e06c7
-
-docker tag gcr.io/tekton-releases/github.com/tektoncd/dashboard/cmd/dashboard@sha256:e72593477e8cb09b718b9b0b38731de5b4cb7ef0c04712f7b97f51be5c5e06c7 $base_repo/tektoncd-dashboard
-
-docker push $base_repo/tektoncd-dashboard
